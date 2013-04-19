2bdd6e84fdd1a70e7189f83f94450af72050e820
Eric Suchyta <eric.d.suchyta@gmail.com>
2013-04-19 09:32:20 -0400

    subimage.py also has a simple function for pasting an image into another image

--- subimage.py
+++ subimage.py
@@ -8,6 +8,13 @@ import pyfits
 def get_subimage(image, region):
     return image[ region[0][0]:(region[1][0]+1), region[0][1]:(region[1][1]+1) ]
 
+#corner = (BL row, BL col)
+def paste_into_image( subimage, image, corner ):
+    height = len(subimage)
+    width = len(subimage[0])
+    image[ corner[0]:(corner[0]+height), corner[1]:(corner[1]+width) ] = image[ corner[0]:(corner[0]+height), corner[1]:(corner[1]+width) ] + subimage
+    return image
+
 
 if __name__=='__main__':
 
@@ -16,6 +23,17 @@ if __name__=='__main__':
     image = hdus[0].data
     hdus.close()
 
+    """
     subimage = get_subimage(image, [ (10000,10000), (12000,12000) ] )
     phdu = pyfits.PrimaryHDU(subimage)
     phdu.writeto('test_subimage.fits')
+    """
+
+    subfile = 'test.fits'
+    hdus = pyfits.open(subfile)
+    subimage = 1000 * hdus[0].data
+    hdus.close()
+
+    image = paste_into_image( subimage, image, (14000,14000) )
+    phdu = pyfits.PrimaryHDU(image)
+    phdu.writeto('test_paste.fits')
