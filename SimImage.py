0f4d9aa2343a87f221399bf7a410a0981404badf
emhuff <emhuff@gmail.com>
2013-04-30 12:31:01 -0400

    SimImage now takes input image of arbitrary size.

--- SimImage.py
+++ SimImage.py
@@ -7,15 +7,13 @@ import math
 import sys
 
 def defineParameters():
-    ngal = 20
+    ngal = 50
     parameters_list = []
     for i in np.arange(ngal):
         parameters = {}
         parameters['Sersic index'] = 4.0
         parameters['half light radius'] = 2.5
         parameters['flux'] = np.random.lognormal(mean=5.,sigma=1.)
-        parameters['xpos'] = np.random.random_sample()*1000
-        parameters['ypos'] = np.random.random_sample()*1000
         parameters['g1'] = 0.1*np.random.randn()
         parameters['g2'] = 0.1*np.random.randn()
         parameters_list.append(parameters)
@@ -34,7 +32,7 @@ def getBigImage(file='example.fits',subRegion= None):
     '''
     Takes a filename for the large image to be simulated. Reads in a
     smaller piece of that image defined (somehow) by subRegion, and
-    returns the subRegion as a GalSim imageF object.
+    returns the subRegion as a GalSim image object.
     '''
     bigImage = galsim.fits.read(file)
     pixelScale = 0.1
@@ -46,8 +44,11 @@ if __name__ == "__main__":
     Takes an input image .fits file name, and output file name, reads in the zeroth
     extension, adds Sersic galaxy images, and then writes the output
     to another .fits file.
+    USAGE:
+       SimImage inputFileName  outputFileName
+
     TODO:
-       Implement subRegion definition
+       Implement subRegion definition.
        Implement psf model and convolution.
     '''
     
@@ -69,8 +70,9 @@ if __name__ == "__main__":
         sersicObj = galsim.Sersic(n=parameters['Sersic index'],half_light_radius=
                                   parameters['half light radius'],flux = parameters['flux'])
         sersicObj.applyShear(g1=parameters['g1'],g2=parameters['g2'])
-        x = parameters['xpos']
-        y = parameters['ypos']
+        x = np.random.random_sample()*bigImage.array.shape[0]
+        y = np.random.random_sample()*bigImage.array.shape[1]
+
         ix = int(np.floor(x+.05))
         iy = int(np.floor(y+.05))
         dx = x-ix
