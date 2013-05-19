b91c82c3fdc6533d1a29e8a8e4637b8e1be0256b
emhuff <emhuff@gmail.com>
2013-05-19 16:42:28 -0400

    now writes the input catalog.

--- balrog.py
+++ balrog.py
@@ -8,18 +8,16 @@ import sys
 from optparse import OptionParser
 import sextractor_engine 
 
-def defineParameters():
-    ngal = 50
-    parameters_list = []
-    for i in np.arange(ngal):
-        parameters = {}
-        parameters['Sersic index'] = 4.0
-        parameters['half light radius'] = 2.5
-        parameters['flux'] = np.random.lognormal(mean=5.,sigma=1.)
-        parameters['g1'] = 0.2*np.random.randn()
-        parameters['g2'] = 0.2*np.random.randn()
-        parameters_list.append(parameters)
-    return parameters_list
+def defineParameters(x=None,y=None):
+    parameters = {}
+    parameters['Sersic index'] = 4.0
+    parameters['half light radius'] = 2.5
+    parameters['flux'] = np.random.lognormal(mean=5.,sigma=1.)
+    parameters['g1'] = 0.2*np.random.randn()
+    parameters['g2'] = 0.2*np.random.randn()
+    parameters['x'] = x
+    parameters['y'] = y
+    return parameters
 
     
 def defineCalibration():
@@ -30,6 +28,23 @@ def defineCalibration():
     calibParams = {}
     calibParams['gain'] = 10000.0
     return calibParams
+
+def writeFitsCatalog(catalog,fileName):
+    '''
+    This assumes that catalog is a list of dictionaries.
+    '''
+    columns = []
+    for key in catalog[0]:
+        # Make an array from all the entries with this key name
+        arr = []
+        for entry in catalog:
+            arr.append(entry[key])
+        arr = np.array(arr)
+        columns.append(pyfits.Column(name=key,format='E',array=arr))
+    tbhdu = pyfits.new_table(pyfits.ColDefs(columns))
+    hdu = pyfits.PrimaryHDU()
+    thdulist = pyfits.HDUList([hdu,tbhdu])
+    thdulist.writeto(fileName)
 
 def getBigImage(file='example.fits',subRegion= (None,None,None,None)):
     '''
@@ -73,27 +88,33 @@ if __name__ == "__main__":
                       type="string",help="Image file to be read",default="example.fits")
     parser.add_option("-o", "--output",action="store",type="string",
                       dest="OutputFile",default="example_output.fits",
-                      help="file to put the output simulated image in.")
+                      help="File to write the output simulated image to.")
     parser.add_option("-w", "--weightin",action="store",type="string",
                       dest="WeightMapIn",default="example_weight_in.fits",
-                      help="file to read the weight map from.")
+                      help="File to read the weight map from.")
     parser.add_option("-v", "--weightout",action="store",type="string",
                       dest="WeightMapOut",default="example_weight_out.fits",
-                      help="file to write the extracted weight map to.")
-    parser.add_option("-t", "--catalogout",action="store",type="string",
+                      help="File to write the extracted weight map to.")
+    parser.add_option("--catalogout",action="store",type="string",
                       dest="CatalogOutFile",default="example_catalog.fits",
-                      help="file to write the SExtractor catalog to.")
+                      help="File to write the SExtractor catalog to.")
+    parser.add_option("--catalogIn",action="store",type="string",
+                      dest="CatalogInFile",default="input_catalog.fits",
+                      help="File to write the input (simulated) catalog to. Must not exist.")
     parser.add_option("-p", "--psfmodel",action="store",type="string",
                       dest="PSFExFile",default="example.psfcat.psf",
                       help="File containing PSFEx psf model to use.")
     parser.add_option("--xmin",action="store",type="int",default="-1",
-                      help="minimum column of extracted subImage, unit-indexed",dest="xmin")
+                      help="Minimum column of extracted subImage, unit-indexed",dest="xmin")
     parser.add_option("--xmax",action="store",type="int",default="-1",
-                      help="maximum column of extracted subImage, unit-indexed",dest="xmax")
+                      help="Maximum column of extracted subImage, unit-indexed",dest="xmax")
     parser.add_option("--ymin",action="store",type="int",default="-1",
-                      help="minimum row of extracted subImage, unit-indexed",dest="ymin")
+                      help="Minimum row of extracted subImage, unit-indexed",dest="ymin")
     parser.add_option("--ymax",action="store",type="int",default="-1",
-                      help="minimum row of extracted subImage, unit-indexed",dest="ymax")
+                      help="Minimum row of extracted subImage, unit-indexed",dest="ymax")
+    parser.add_option("--ngal",action="store",type="int",default="50",
+                      help="Number of simulated galaxies to add.",dest="ngal")
+
 
     (opts,args ) = parser.parse_args()
 
@@ -104,20 +125,21 @@ if __name__ == "__main__":
     subRegion = (opts.xmin,opts.xmax,opts.ymin,opts.ymax)
 
     rng = galsim.UniformDeviate()
-    parameters_list = defineParameters()
     calib = defineCalibration()
     bigImage, offset = getBigImage(opts.ImageFile,subRegion=subRegion)
     psfmodel = galsim.des.DES_PSFEx(opts.PSFExFile)
     center = bigImage.bounds.center()
-    
+    inputCatalog =[]
 
-    for parameters in parameters_list:
+    for i in range(opts.ngal):
+        x = np.random.random_sample()*bigImage.array.shape[0]
+        y = np.random.random_sample()*bigImage.array.shape[1]
+        parameters = defineParameters(x=x,y=y)
+        inputCatalog.append(parameters)
         sersicObj = galsim.Sersic(n=parameters['Sersic index'],half_light_radius=
                                   parameters['half light radius'],flux = parameters['flux'])
         sersicObj.applyShear(g1=parameters['g1'],g2=parameters['g2'])
-        x = np.random.random_sample()*bigImage.array.shape[0]
-        y = np.random.random_sample()*bigImage.array.shape[1]
-
+        
         ix = int(np.floor(x+.05))
         iy = int(np.floor(y+.05))
         dx = x-ix
@@ -136,7 +158,9 @@ if __name__ == "__main__":
         
         bigImage[bounds] += smallImage[bounds]
 
-    
+    # Record the catalog of generated objects.
+    writeFitsCatalog(inputCatalog,opts.CatalogInFile)
+    # Write the subImage file.
     bigImage.write(opts.OutputFile)
     # Next, write the subImage weightmap.
     subWeight, Wcent = getBigImage(opts.WeightMapIn,subRegion=subRegion)
