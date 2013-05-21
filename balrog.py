3514117b2c4712f0a059886065378afaebc33f8c
emhuff <emhuff@gmail.com>
2013-05-21 15:45:59 -0400

    Tried writing psf images to file, skilling the galaxies.

--- balrog.py
+++ balrog.py
@@ -152,9 +152,9 @@ if __name__ == "__main__":
         psf = psfmodel.getPSF(pos-offset,bigImage.getScale())
         finalPSF = galsim.Convolve([pix,psf])
         sersicObjConv = galsim.Convolve([finalPSF,sersicObj])
-        smallImage = finalPSF.ImageD(int(np.ceil(5*parameters['half light radius'])),
+        smallImage = galsim.ImageD(int(np.ceil(5*parameters['half light radius'])),
                                    int(np.ceil(5*parameters['half light radius'])))
-        smallImage = sersicObjConv.draw(dx=bigImage.getScale(),image=smallImage)
+        smallImage = finalPSF.draw(dx=bigImage.getScale(),image=smallImage)
         smallImage.addNoise(galsim.CCDNoise(gain=calib['gain'],read_noise=0))
         smallImage.setCenter(ix,iy)
         bounds = smallImage.bounds & bigImage.bounds
