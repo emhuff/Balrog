d7169bc36c4fec4c50c1b3b98aacbbba27e85555
suchyta1 <eric.d.suchyta@gmail.com>
2014-02-14 18:00:37 -0500

    removed unneccesary stuff

--- nogit/benchmark.sh
+++
@@ -1,5 +0,0 @@
-#./RunSextractorBalrog2.py --nsim 1 --clean --ngal 1000 --sexpath $SV_CLUSTERS/pipeline/astro_software/sextractor-trunk-multithreaded_install/bin/sex --nthreads 1
-#./RunSextractorBalrog2.py --nsim 2 --clean --ngal 1000 --sexpath $SV_CLUSTERS/pipeline/astro_software/sextractor-trunk-multithreaded_install/bin/sex --nthreads 2
-#./RunSextractorBalrog2.py --nsim 4 --clean --ngal 1000 --sexpath $SV_CLUSTERS/pipeline/astro_software/sextractor-trunk-multithreaded_install/bin/sex --nthreads 4
-./RunSextractorBalrog2.py --nsim 8 --clean --ngal 1000 --sexpath $SV_CLUSTERS/pipeline/astro_software/sextractor-trunk-multithreaded_install/bin/sex --nthreads 8
-./RunSextractorBalrog2.py --nsim 12 --clean --ngal 1000 --sexpath $SV_CLUSTERS/pipeline/astro_software/sextractor-trunk-multithreaded_install/bin/sex --nthreads 12
