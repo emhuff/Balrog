fc4717ae0c627434108d1853adecd2d3e377ff55
NiallMac <niall.maccrann@gmail.com>
2015-08-07 04:01:59 -0700

    now using galsim for all wcs

--- multi_epoch_tools/coords.py
+++ multi_epoch_tools/coords.py
@@ -1,12 +1,24 @@
-import pywcs
+import astropy.wcs as apywcs
 import galsim
 import numpy as np
-
+"""
 def get_wcoords(x, y, header):
     wcs = pywcs.WCS(header)
     pcoords = np.dstack((x,y))[0]
     wcoords = wcs.wcs_pix2sky(pcoords, 1)
     return wcoords.T
+"""
+def get_wcoords(xs, ys, wcs):
+    wcoords=np.zeros((2,xs.shape[0]))
+    for i,(x,y) in enumerate(zip(xs,ys)):
+        print type(x),type(y)
+        im_pos=galsim.PositionD(np.float(x),np.float(y))
+        wcoords_galsim=wcs.toWorld(im_pos)
+        print wcoords_galsim.ra,wcoords_galsim.dec
+        wcoords[0,i],wcoords[1,i]=wcoords_galsim.ra/galsim.degrees,wcoords_galsim.dec/galsim.degrees
+    return wcoords
+
+
 
 def get_pcoords(ra, dec, wcs):
     #wcs=galsim.wcs.readFromFitsHeader(header)[0]                                                                              
